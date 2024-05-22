9384b9ca0deceb4dbf2939dc013e471a473544c3
d <153951662+dtplsongithub@users.noreply.github.com>
2024-05-22 10:42:38 +0300

    oop wrong file

--- update.bat
+++ update.bat
@@ -1,6 +1,11 @@
+: updater
+
+
 @echo off
 if exist temp\ (
   echo Temp folder detected.
+  rd /S /Q "temp\"
+  mkdir temp
 ) else (
   echo Temp folder not detected. Creating one...
   mkdir temp
@@ -9,13 +14,31 @@ cd temp
 if exist latestversion.zip (
   del latestversion.zip
 )
-echo Downloading latest version...
-powershell -Command "Invoke-WebRequest https://github.com/dtplsongithub/ebgg/releases/latest/download/windows-amd64.zip -OutFile latestversion.zip"
-
-cd..
+cls
+echo.
+echo What kind of ebgg version do you have?
+echo.
+echo 1) windows-amd64
+echo 2) windows-amd64-openJDK-included
+echo.
+choice /C 12 /N
+cls
+if %ERRORLEVEL% EQU 2 (
+  echo Downloading latest version...
+  powershell Invoke-WebRequest https://github.com/dtplsongithub/ebgg/releases/latest/download/windows-amd64-openJDK-included.zip -OutFile   latestversion.zip
+  echo %ERRORLEVEL%
+  powershell Expand-Archive latestversion.zip -DestinationPath .\
+  xcopy ".\windows-amd64-openJDK-included\" "..\" /L /Y /E
+  echo %ERRORLEVEL%
+) else if %ERRORLEVEL% EQU 1 (
+  echo Downloading latest version...
+  powershell Invoke-WebRequest https://github.com/dtplsongithub/ebgg/releases/latest/download/windows-amd64.zip -OutFile   latestversion.zip
+  powershell Expand-Archive latestversion.zip -DestinationPath .\
+  cd..
 dir
-pause
-xcopy "temp\windows-amd64\" ".\" /C /L
+  xcopy ".\temp\windows-amd64\" ".\" /L /Y /E
+)
+echo Succesfully updated! Press any key to exit...
+pause>nul
+cd..
 rd /S /Q "temp\"
-echo Succesfully updated!
-pause
